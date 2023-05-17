b02f6154a6f29d8feac716d25114df57c8f5d605
BrandonWu500 <brwu17@gmail.com>
2023-05-17 16:45:11 -0400

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <div>
+      <MarketingApp />
+    </div>
+  );
+};
+export default App;
---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import React, { useRef, useEffect } from "react";
+import { mount as marketingMount } from "marketing/MarketingApp";
+
+const MarketingApp = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    marketingMount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
+export default MarketingApp;
